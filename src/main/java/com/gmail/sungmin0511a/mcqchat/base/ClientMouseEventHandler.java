2acac5ad427e6705954598712d57025c34aeede0
HolyGuy6455 <sungmin0511a@gmail.com>
2019-01-29 16:47:19 +0900

    toolbar image has changed and quick chat button ui is updated. there is no more ump9 chan.

--- src/main/java/com/gmail/sungmin0511a/mcqchat/base/ClientMouseEventHandler.java
+++ src/main/java/com/gmail/sungmin0511a/mcqchat/base/ClientMouseEventHandler.java
@@ -1,5 +1,8 @@
 package com.gmail.sungmin0511a.mcqchat.base;
 
+import com.gmail.sungmin0511a.mcqchat.gui.ClientToolbarGUI;
+
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -7,13 +10,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  * ClientMouseEventHandler
  */
 public class ClientMouseEventHandler {
-    private static int MOUSE_EVENT_MOVE = -1;
-    private static int MOUSE_EVENT_WHEEL_CLICKED = -1;
+    public static int MOUSE_EVENT_MOVE = -1;
+    public static int MOUSE_EVENT_WHEEL_CLICK = 2;
 
     @SubscribeEvent
     public void onMouseEvent(MouseEvent e) {
         if(e.getButton() == MOUSE_EVENT_MOVE){
             return;
+        }else if(e.getButton() == MOUSE_EVENT_WHEEL_CLICK){
+            Minecraft.getMinecraft().displayGuiScreen(new ClientToolbarGUI());
         }
         QuickChatMod.logger.info("on mouse event listen");
         QuickChatMod.logger.info("event is : {}",e.getButton());
--- src/main/java/com/gmail/sungmin0511a/mcqchat/gui/ClientToolbarGUI.java
+++ src/main/java/com/gmail/sungmin0511a/mcqchat/gui/ClientToolbarGUI.java
@@ -2,11 +2,13 @@ package com.gmail.sungmin0511a.mcqchat.gui;
 
 import java.io.IOException;
 
+import com.gmail.sungmin0511a.mcqchat.base.ClientMouseEventHandler;
 import com.gmail.sungmin0511a.mcqchat.base.QuickChatMod;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
 /**
@@ -15,19 +17,33 @@ import net.minecraft.util.ResourceLocation;
 public class ClientToolbarGUI extends GuiScreen {
 
     private ResourceLocation resourceLocation;
-    final private int GUI_WIDTH = 186;
-    final private int GUI_HEIGHT = 256;
+    final private int GUI_WIDTH = 28;
+    final private int GUI_HEIGHT = 28;
+    final private int SHT = 29; // short reach
+    final private int LNG = 60; // long reach
+    final private int DRAW_LOCATION_X[] = {-SHT, 1  , SHT,-LNG, LNG,-SHT, 0  , SHT};
+    final private int DRAW_LOCATION_Y[] = {-SHT,-LNG,-SHT, 0  , 0  , SHT, LNG, SHT};
 
-    @Override
-    public void initGui() {
-        super.initGui();
+    public ClientToolbarGUI(){
         resourceLocation = new ResourceLocation(QuickChatMod.MODID, "textures/gui/toolbar.png");
     }
 
     @Override
+    public void initGui() {
+        super.initGui();
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
         Minecraft.getMinecraft().renderEngine.bindTexture(resourceLocation);
-        drawTexturedModalRect(0, 0, 0, 0, GUI_WIDTH, GUI_HEIGHT);
+        GlStateManager.translate(width/2,height/2,0);
+        GlStateManager.scale(2,2,2);
+        
+        for (int i = 0; i < 8; i++) {
+            drawTexturedModalRect(DRAW_LOCATION_X[i]-14, DRAW_LOCATION_Y[i]-14,0, 0, GUI_WIDTH, GUI_HEIGHT);
+        }
+        
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -44,5 +60,18 @@ public class ClientToolbarGUI extends GuiScreen {
     @Override
     public boolean doesGuiPauseGame() {
         return false;
+    }
+
+    @Override
+    protected void mouseReleased(int mouseX, int mouseY, int state) {
+        super.mouseReleased(mouseX, mouseY, state);
+        QuickChatMod.logger.info("state : {}",state);
+        if(state == ClientMouseEventHandler.MOUSE_EVENT_WHEEL_CLICK){
+            //quit
+            //Minecraft.getMinecraft().displayGuiScreen(null);
+        }else{
+            sendChatMessage("wow rywly?");
+        }
+        
     }
 }
